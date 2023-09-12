e4b1a30c9c36fde396af47b14704dddc48f607bc
amir naderi <63848506+amir-ndr@users.noreply.github.com>
2023-09-12 04:44:28 +0330

    u

--- trainer.py
+++ trainer.py
@@ -1,13 +1,3 @@
-# Based on the code of Monodepth2
-# https://github.com/nianticlabs/monodepth2/blob/master/trainer.py
-
-# Copyright Niantic 2019. Patent Pending. All rights reserved.
-#
-# This software is licensed under the terms of the Monodepth2 licence
-# which allows for non-commercial use only, the full terms of which are made
-# available in the LICENSE file.
-
-
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
